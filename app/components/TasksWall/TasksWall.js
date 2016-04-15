40082d692ee269c5b8542253931d045768d1a493
Juan Bernardo <jrivera@hugeinc.com>
2016-04-15 09:26:07 -0500

    working with classname

--- app/components/TasksWall/TasksWall.js
+++ app/components/TasksWall/TasksWall.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import css from './TasksWall.scss';
+import styles from './TasksWall.scss';
+import classNames from 'classnames/bind';
 
 import Task from '../Task/Task';
+
+const css = classNames.bind(styles);
 
 const TasksWall = ({ toDo, inProgress, done }) => {
 
   return (
-    <div className={ css['tasks-wall'] }>
-      <div className={ css['task-group'] }>
-        <h2 className={ css['task-group-title'] }>toDo</h2>
-        <ul className={ css['task-group-list'] }>
+    <div className={ css('tasks-wall') }>
+      <div className={ css('task-group', 'toDo') }>
+        <h2 className={ css('task-group-title') }>toDo</h2>
+        <ul className={ css('task-group-list') }>
           { toDo.map(Task) }
         </ul>
       </div>
-      <div className={ css['task-group'] }>
-        <h2 className={ css['task-group-title'] }>inProgress</h2>
-        <ul className={ css['task-group-list'] }>
+      <div className={ css('task-group', 'inProgress') }>
+        <h2 className={ css('task-group-title') }>inProgress</h2>
+        <ul className={ css('task-group-list') }>
           { inProgress.map(Task) }
         </ul>
       </div>
-      <div className={ css['task-group'] }>
-        <h2 className={ css['task-group-title'] }>Done</h2>
-        <ul className={ css['task-group-list'] }>
+      <div className={ css('task-group', 'done') }>
+        <h2 className={ css('task-group-title') }>Done</h2>
+        <ul className={ css('task-group-list') }>
           { done.map(Task) }
         </ul>
       </div>
