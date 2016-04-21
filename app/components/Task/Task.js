854bb3c286bf8d3c4e8ca48cc48c7b114eefbab9
Juan Bernardo <jrivera@hugeinc.com>
2016-04-21 18:40:09 -0500

    partial work

--- app/components/Task/Task.js
+++ app/components/Task/Task.js
@@ -8,8 +8,10 @@ const css = classNames.bind(styles);
 const Task = ({ id, title, description }, i) => {
   return (
     <li key={ id } className={ css('task-item') } data-id={ id } data-index={ i }>
-      <a href={ `#${id}` } className={ css('task-item-title', 'js-task-title') } draggable="false">{ title }</a>
-      <p className={ css('task-item-description', 'js-task-description') }>{ description }</p>
+      <div className={ css('task-item-box', 'js-item-box') }>
+        <a href={ `#${id}` } className={ css('task-item-title', 'js-task-title') } draggable="false">{ title }</a>
+        <p className={ css('task-item-description', 'js-task-description') }>{ description }</p>
+      </div>
     </li>
   );
 }
--- app/components/TasksWall/TasksWall.js
+++ app/components/TasksWall/TasksWall.js
@@ -33,22 +33,23 @@ const TasksWall = ({
     }
   }
 
-  function onMouseMove(task) {
+  function onMouseMove(box) {
     let x = 0,
       y = 0,
-      left = getOffsetLeft(task) + task.offsetWidth / 2,
-      top = getOffsetTop(task) - 10;
+      left = getOffsetLeft(box) + box.offsetWidth / 2,
+      top = getOffsetTop(box) - 10;
 
     return function mouseMove(e) {
       x = e.x - left;
       y = e.y - top + document.scrollingElement.scrollTop;
-      task.style.transform = `translate(${x}px, ${y}px)`;
+      box.style.transform = `translate(${x}px, ${y}px)`;
     }
   }
 
   function onMouseDown(e) {
     const task = findTask(e.target);
     if (task) {
+      const box = task.querySelector('.js-item-box')
       let data = {
         elem: task,
         groupFrom: findGroup(task),
@@ -56,10 +57,10 @@ const TasksWall = ({
         title: task.querySelector('.js-task-title').innerHTML,
         description: task.querySelector('.js-task-description').innerHTML
       };
-      task.style.position = 'relative';
+      box.style.position = 'relative';
       document.body.style.cursor = 'move';
       e.currentTarget.addEventListener('mouseup', onMouseUp(data));
-      e.currentTarget.onmousemove = onMouseMove(task);
+      e.currentTarget.onmousemove = onMouseMove(box);
     }
   }
 
