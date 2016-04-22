2d4c37165790802668d58e609707965f3b48f640
Juan Bernardo <jrivera@hugeinc.com>
2016-04-22 11:47:26 -0500

    feature: on drag shadow improvement

--- app/components/Task/Task.js
+++ app/components/Task/Task.js
@@ -5,6 +5,8 @@ import classNames from 'classnames/bind';
 
 const css = classNames.bind(styles);
 
+const TASKS_MARGINBOTTOM = 7;
+
 const Task = ({ id, title, description }, i) => {
 
   function onMouseOut(e) {
@@ -16,14 +18,14 @@ const Task = ({ id, title, description }, i) => {
   function onMouseEnter(e) {
     const task = e.target.parentNode;
     const height = task.offsetHeight;
-    task.style.padding = `${height}px 0 0`;
+    task.style.padding = `${height + TASKS_MARGINBOTTOM}px 0 0`;
     task.firstChild.style.height = `${height}px`;
     task.addEventListener('mouseout', onMouseOut);
   }
 
   return (
     <li key={ id } className={ css('task-item') } data-id={ id } data-index={ i }>
-      <div className={ css('fake-mouseenter', 'fake-ondrag') } onMouseEnter={ onMouseEnter }></div>
+      <div className={ css('fake-ondrag') } onMouseEnter={ onMouseEnter }></div>
       <div className={ css('task-item-content', 'js-item-content') }>
         <a href={ `#${id}` } className={ css('task-item-title', 'js-task-title') } draggable="false">{ title }</a>
         <p className={ css('task-item-description', 'js-task-description') }>{ description }</p>
--- app/components/TasksWall/TasksWall.js
+++ app/components/TasksWall/TasksWall.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import styles from './TasksWall.scss';
+import taskStyles from '../Task/Task.scss';
 import classNames from 'classnames/bind';
 import {
   addToDo,
@@ -13,6 +14,7 @@ import { getOffsetLeft, getOffsetTop } from '../../js/getOffset';
 import TaskGroup from '../TaskGroup/TaskGroup';
 
 const css = classNames.bind(styles);
+const taskCss = classNames.bind(taskStyles);
 
 const TasksWall = ({
   tasks,
@@ -30,7 +32,7 @@ const TasksWall = ({
       elem.style.transform = 'none';
       elem.style.position = 'static';
       elem.parentNode.firstChild.style.removeProperty('display');
-      document.body.classList.remove('drag-active');
+      document.body.classList.remove(taskCss('drag-active'));
       e.currentTarget.removeEventListener('mouseup', mouseUp);
       e.currentTarget.onmousemove = null;
       if (groupFrom !== groupTo) {
@@ -41,10 +43,11 @@ const TasksWall = ({
 
   function onMouseMove(content) {
     let x = 0,
-      y = 0;
+      y = 0,
+      left = getOffsetLeft(content) + content.offsetWidth / 2;
+
     return function mouseMove(e) {
-      let left = getOffsetLeft(content) + content.offsetWidth / 2,
-        top = getOffsetTop(content) - 10;
+      let top = getOffsetTop(content) - 10;
       x = e.x - left;
       y = e.y - top + document.scrollingElement.scrollTop;
       content.style.transform = `translate(${x}px, ${y}px)`;
@@ -64,8 +67,9 @@ const TasksWall = ({
       };
       content.style.position = 'relative';
       task.firstChild.style.display = 'none';
+      task.style.background = 'rgba(0, 0, 0, 0.1)';
       document.body.style.cursor = 'move';
-      document.body.classList.add('drag-active');
+      document.body.classList.add(taskCss('drag-active'));
       e.currentTarget.addEventListener('mouseup', onMouseUp(data));
       e.currentTarget.onmousemove = onMouseMove(content);
     }
