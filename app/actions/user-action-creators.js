b1a49f1f03416de87b30c757e0cb594885298eab
Juan Bernardo <jrivera@hugeinc.com>
2016-05-09 23:40:10 -0500

    facebooklogin action creator

--- app/actions/user-action-creators.js
+++ app/actions/user-action-creators.js
@@ -10,15 +10,39 @@ export function getUserAsync() {
   return (dispatch) => {
     FB.getLoginStatus(function(response) {
       if (response.status === 'connected') {
-        var uid = response.authResponse.userID;
-        console.log(response);
-        var accessToken = response.authResponse.accessToken;
+        
       } else if (response.status === 'not_authorized') {
         console.log('not_authorized');
       } else {
         console.log('isnt logged in to facebook');
       }
       dispatch(getUser(response));
+    });
+  };
+}
+
+export const FACEBOOK_LOGIN = 'FACEBOOK_LOGIN';
+
+function facebookLogin(response) {
+  return {
+    type: FACEBOOK_LOGIN, response
+  };
+}
+
+function facebookApi(dispatch) {
+  FB.api('/me', function(response) {
+    dispatch(facebookLogin(response));
+  });
+}
+
+export function facebookLoginAsync() {
+  return (dispatch) => {
+    FB.login(function(response) {
+      if (response.authResponse) {
+        facebookApi(dispatch);
+      } else {
+       console.log('User cancelled login or did not fully authorize.');
+      }
     });
   };
 }
