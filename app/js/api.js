9356531bf7bbc263a54a66e3eedfd755b3e933e3
Juan Bernardo <jrivera@hugeinc.com>
2016-05-31 08:17:58 -0500

    state.user.res validation

--- app/js/api.js
+++ app/js/api.js
@@ -73,11 +73,13 @@ function setFireBaseTasks(next, action, store) {
   next(action);
   const state = Object.assign({}, store.getState());
 
-  database.ref(`users/${state.user.res.uid}/tasks`).set(
-    Object.keys(state.tasks).reduce((a, b, i) => {
-      return a[i] = { name: b, data: state.tasks[b] }, a;
-    }, [])
-  );
+  if (state.user.res) {
+    database.ref(`users/${state.user.res.uid}/tasks`).set(
+      Object.keys(state.tasks).reduce((a, b, i) => {
+        return a[i] = { name: b, data: state.tasks[b] }, a;
+      }, [])
+    );
+  }
 }
 
 const firebaseMiddleware = store => next => {
