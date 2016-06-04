87cbcc3e7a7a00253d7be3ead3472a5b8e919615
Juan Bernardo <jrivera@hugeinc.com>
2016-06-04 09:34:23 -0500

    TaskGroup as class component

--- app/components/TaskGroup/TaskGroup.js
+++ app/components/TaskGroup/TaskGroup.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './TaskGroup.scss';
 import classNames from 'classnames/bind';
 
@@ -7,42 +7,70 @@ const css = classNames.bind(styles);
 import Task from '../Task/Task';
 import NewTask from '../NewTask/NewTask';
 
-const TaskGroup = ({ array, name, addbtn, editMode }) => {
+import resetInlineStyles from '../../js/resetInlineStyles';
 
-  function onSubmit(e) {
-    e.preventDefault();
+class TaskGroup extends Component {
+
+  constructor (props) {
+    super(props);
+    this.onCancel = this.onCancel.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  return (
-    <div className={ css('task-group', 'js-task-group') } data-group={ name }>
-      {
-        editMode ?
-          <form className={ css('edit-form') } onSubmit={ onSubmit }>
-            <input className={ css('edit-group-title') } name="groupTitle" type="text" defaultValue={name} />
-            <div className={ css('centered-btns') }>
-              <input className={ css('save-btn', 'btn') } type="submit" name="save" value="save"></input>
-              <span className={ css('cancel-btn', 'btn') } onClick={ onCancel(name) }>cancel</span>
-            </div>
-          </form>
-         :
-        <h2 className={ css('task-group-title') }>
-         { name }
-         {
-           addbtn ? <NewTask /> : ''
-         }
-       </h2>
-      }
-      <ul className={ css('task-group-list', name) }>
-        { array.map(Task) }
-      </ul>
-    </div>
-  );
-};
+  onSubmit (e) {
+    e.preventDefault();
+    resetInlineStyles(this.groupTitle);
+    this.btns.style.display = 'none';
+  }
 
-function onCancel(name) {
-  return function(e) {
-    e.target.parentNode.parentNode.groupTitle.value = name;
-  };
-}
+  onChange (e) {
+    if (!e.target.style.paddingRight) {
+      e.target.style.paddingRight = '40px';
+      this.btns.style.display = 'block';
+    }
+  }
+
+  onCancel (name) {
+    return (e) => {
+      this.groupTitle.value = name;
+      resetInlineStyles(this.groupTitle);
+      this.btns.style.display = 'none';
+    };
+  }
+
+  render () {
+    const { array, name, addbtn, editMode } = this.props;
+    return (
+      <div className={ css('task-group', 'js-task-group') } data-group={ name }>
+        {
+          editMode ?
+            <form className={ css('edit-form') } onSubmit={ this.onSubmit }>
+              <input
+                className={ css('edit-group-title') }
+                type="text"
+                defaultValue={name}
+                onChange={ this.onChange }
+                ref={ (ref) => this.groupTitle = ref } />
+              <div className={ css('centered-btns') } ref={ (ref) => this.btns = ref } >
+                <input className={ css('save-btn', 'btn') } type="submit" name="save" value="save"></input>
+                <span className={ css('cancel-btn', 'btn') } onClick={ this.onCancel(name) }>cancel</span>
+              </div>
+            </form>
+           :
+          <h2 className={ css('task-group-title') }>
+           { name }
+           {
+             addbtn ? <NewTask /> : ''
+           }
+         </h2>
+        }
+        <ul className={ css('task-group-list', this.name) }>
+          { array.map(Task) }
+        </ul>
+      </div>
+    );
+  }
+};
 
 export default TaskGroup;
