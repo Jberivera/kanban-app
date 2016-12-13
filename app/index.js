799c4510be1d75659677d029541de8d3c9d30564
Juan Bernardo <jrivera@hugeinc.com>
2016-12-13 07:56:17 -0500

    configureStore

--- app/index.js
+++ app/index.js
@@ -4,27 +4,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
-import createLogger from 'redux-logger';
-import firebaseMiddleware from './js/api';
+import { syncHistoryWithStore } from 'react-router-redux';
+import configureStore from './js/redux/configureStore';
 
-// reducers
-import kanbanApp from './reducers/kanbanApp';
-
-const reducer = combineReducers(Object.assign({}, kanbanApp, {
-  routing: routerReducer
-}));
-
-const store = createStore(
-  reducer,
-  applyMiddleware(
-    thunk,
-    firebaseMiddleware,
-    createLogger()
-  )
-);
+const store = configureStore();
 
 const history = syncHistoryWithStore(browserHistory, store);
 
@@ -34,12 +18,12 @@ import { TasksWall, EditProject, Root } from './components';
 const App = (props) => {
 
   return (
-    <Provider store={store}>
-      <Router history={history}>
-        <Route path="/" component={Root}>
-          <IndexRoute component={TasksWall} />
-          <Route path="/wall" component={TasksWall}/>
-          <Route path="/edit-mode" component={EditProject}/>
+    <Provider store={ store }>
+      <Router history={ history }>
+        <Route path="/" component={ Root }>
+          <IndexRoute to={ TasksWall } />
+          <Route path="/wall" component={ TasksWall }/>
+          <Route path="/edit-mode" component={ EditProject }/>
         </Route>
       </Router>
     </Provider>
