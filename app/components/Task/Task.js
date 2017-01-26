40c2f78a8f06357ae958d7a5940cb922468d8615
Juan Bernardo <jrivera@hugeinc.com>
2017-01-25 20:30:51 -0500

    Task as class

--- app/components/Task/Task.js
+++ app/components/Task/Task.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './Task.scss';
 import classNames from 'classnames/bind';
 import EditTask from '../EditTask/EditTask';
@@ -7,32 +7,41 @@ const css = classNames.bind(styles);
 
 const TASKS_MARGINBOTTOM = 7;
 
-const Task = ({ id, title, description, i }) => {
-
-  function onMouseOut(e) {
-    const task = e.target.parentNode;
-    task.style.padding = '0';
-    task.removeEventListener('mouseout', onMouseOut);
+class Task extends Component {
+  constructor (props) {
+    super(props);
+    this.mouseOutHandler = this.mouseOutHandler.bind(this);
+    this.mouseEnterHandler = this.mouseEnterHandler.bind(this);
   }
 
-  function onMouseEnter(e) {
+  mouseOutHandler (e) {
+    const task = e.target.parentNode;
+    task.style.padding = '0';
+    task.removeEventListener('mouseout', this.mouseOutHandler);
+  }
+
+  mouseEnterHandler (e) {
     const task = e.target.parentNode;
     const height = task.offsetHeight;
     task.style.padding = `${height + TASKS_MARGINBOTTOM}px 0 0`;
     task.firstChild.style.height = `${height}px`;
-    task.addEventListener('mouseout', onMouseOut);
+    task.addEventListener('mouseout', this.mouseOutHandler);
   }
 
-  return (
-    <li className={ css('task-item', 'g-task-item') } data-id={ id } data-index={ i }>
-      <div className={ css('fake-ondrag', 'g-fake-ondrag') } onMouseEnter={ onMouseEnter }></div>
-      <div className={ css('task-item-content', 'js-item-content') }>
-        <a href={ `#${id}` } className={ css('task-item-title', 'js-task-title') } draggable="false">{ title }</a>
-        <p className={ css('task-item-description', 'js-task-description') }>{ description }</p>
-      </div>
-      <EditTask id={ id } title={ title } description={ description } />
-    </li>
-  );
+  render () {
+    const { id, title, description, i } = this.props;
+
+    return (
+      <li className={ css('task-item', 'g-task-item') } data-id={ id } data-index={ i }>
+        <div className={ css('fake-ondrag', 'g-fake-ondrag') } onMouseEnter={ this.mouseEnterHandler }></div>
+        <div className={ css('task-item-content', 'js-item-content') }>
+          <a href={ `#${id}` } className={ css('task-item-title', 'js-task-title') } draggable="false">{ title }</a>
+          <p className={ css('task-item-description', 'js-task-description') }>{ description }</p>
+        </div>
+        <EditTask id={ id } title={ title } description={ description } />
+      </li>
+    );
+  }
 }
 
 export default Task;
